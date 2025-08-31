bcb9a98c6d1bd302bc2fe2c0d2e9f6755d8b69fe
Beardgit <sanych.beard@gmail.com>
2025-08-31 21:06:56 +0300

    Добавить конструктор в ValidatorDateRelease

--- src/main/java/ru/yandex/practicum/filmorate/model/ValidatorDateRelease.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ValidatorDateRelease.java
@@ -2,9 +2,11 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
 // данный клас будет реализовывать интерфейс ConstraintValidator для работы с аннотацией DateValidator  и проверки значений типа LocalDate
 class ValidatorDateRelease implements ConstraintValidator<DateValidator, LocalDate> {
     private static final LocalDate EARLIEST_DATE = LocalDate.of(1895, 12, 28);
